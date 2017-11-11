ec2776d659ebc1eb0c1e1efe7b12f6789ecba4de
gtsofa <jlsnyule@yahoo.com>
2017-11-11 12:50:21 +0300

    add implement code on person and room class

---
+++ models/testPerson.py
@@ -0,0 +1,7 @@
+import unittest
+
+from person import Person
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ models/testRoom.py
@@ -0,0 +1,7 @@
+import unittest
+
+from room import Room
+
+
+if __name__ == "__main__":
+    unittest.main()
